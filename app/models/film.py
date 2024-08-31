13e70917771a1ce2c9fb15c14da4fde82e4b8557
tamara <ts@b2basket.ru>
2024-08-31 12:23:50 +0300

    добавлены первоначальные модели

--- app/models/film.py
+++ app/models/film.py
@@ -1,10 +1,25 @@
+from datetime import datetime
 from typing import Optional
 from uuid import UUID
-
 from models.base import OrjsonBaseModel
 
+class Genre(OrjsonBaseModel):
+    id: str
+    name: str
+    description: Optional[str] = None
+
+class Person(OrjsonBaseModel):
+    id: str
+    full_name: str
+    role: Optional[str] = None
 
 class Film(OrjsonBaseModel):
-    id: UUID
+    id: str
     title: str
-    imdb_rating: Optional[float]
+    description: Optional[str] = None
+    creation_date: Optional[datetime] = None
+    file_path: Optional[str] = None
+    rating: Optional[float] = None
+    type: str
+    genres: list[Genre]
+    persons: list[Person]
